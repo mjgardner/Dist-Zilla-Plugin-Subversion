116b839db1129a0c532ee1a00660408b89ef4163
gardnerm <gardnerm@01a83866-e137-448d-94ba-132f31b327cc>
2010-03-26 17:59:30 +0000

    refactor into role for general Subversion actions and plugin for tagging - still need plugin for copying dist
    git-svn-id: http://devsvn.gspt.net/svn/operations/shared_services/Dist-Zilla-Plugin-ImportToSubversion/trunk@2441 01a83866-e137-448d-94ba-132f31b327cc

---
+++ lib/Dist/Zilla/Plugin/Subversion/Tag.pm
@@ -0,0 +1,91 @@
+package Dist::Zilla::Plugin::Subversion::Tag;
+
+# ABSTRACT: tags a distribution in Subversion
+
+# last changed on:
+# $Date$
+# $Revision$
+# $HeadURL$
+# $Author$
+
+use Moose;
+with 'Dist::Zilla::Role::Subversion';
+with 'Dist::Zilla::Role::AfterRelease';
+
+use Cwd;
+use English qw(-no_match_vars);
+use Modern::Perl;
+use Moose::Util::TypeConstraints 'find_type_constraint';
+use MooseX::Types::URI 'Uri';
+use SVN::Client;
+use namespace::autoclean;
+
+=attr tag_url
+
+URL for the directory receiving tags for your distribution.  During release
+this will be appended with a directory named with your distribution's name
+and version number.
+
+=cut
+
+has 'tag_url' => (
+    is         => 'ro',
+    isa        => Uri,
+    coerce     => 1,
+    lazy_build => 1,
+);
+
+sub _build_tag_url {
+    my $url = $ARG[0]->_base_url();
+    $url->path_segments( $url->path_segments(), 'tags' );
+    return $url;
+}
+
+=method after_release
+
+Implemented for
+L<Dist::Zilla::Role::AfterRelease|Dist::Zilla::Role::AfterRelease> role.
+Copies the working copy to a tag named after the distribution and its version.
+
+=cut
+
+sub after_release {
+    my $self = shift;
+    my ( $working_url, $tag_url )
+        = map { $self->$ARG } qw(working_url tag_url);
+    my %meta = %{ $self->zilla->distmeta() };
+
+    $tag_url->path_segments( $tag_url->path_segments(),
+        join '-', @meta{qw(name version)} );
+    $self->log("Tagging $working_url as $tag_url");
+
+    if ( my $commit_info = $self->_svn->commit( getcwd(), 0 ) ) {
+        $self->log( $commit_info->author()
+                . " committed working copy on "
+                . $commit_info->date() );
+        if ( $commit_info
+            = $self->_svn->copy( "$working_url", 'HEAD', "$tag_url" ) )
+        {
+            $self->log( $commit_info->author()
+                    . " tagged $working_url as $tag_url on "
+                    . $commit_info->date() );
+            return;
+        }
+    }
+
+    $self->log_fatal("Failed tag of $working_url as $tag_url");
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
+__END__
+
+=head1 DESCRIPTION
+
+This L<Dist::Zilla|Dist::Zilla> after-release plugin can be used to tag your
+distribution in Subversion.
+In addition to the attributes listed here, it can be configured with
+attributes from
+L<Dist::Zilla::Role::Subversion|Dist::Zilla::Role::Subversion>.
---
+++ lib/Dist/Zilla/Role/Subversion.pm
@@ -0,0 +1,180 @@
+package Dist::Zilla::Role::Subversion;
+
+# ABSTRACT: does Subversion actions for a distribution
+
+# last changed on:
+# $Date$
+# $Revision$
+# $HeadURL$
+# $Author$
+
+use Moose::Role;
+with 'Dist::Zilla::Role::Plugin';
+
+use Cwd;
+use English qw(-no_match_vars);
+use Modern::Perl;
+use MooseX::Types::URI 'Uri';
+use Path::Class qw(dir file);
+use Readonly;
+use Regexp::DefaultFlags;
+use SVN::Client;
+use SVN::Wc;
+use namespace::autoclean;
+
+=attr svn_user
+
+Your Subversion user ID.  Defaults to the cached credentials for your
+distribution's working copy.
+
+=attr svn_password
+
+Your Subversion password.  Defaults to the cached credentials for your
+distribution's working copy.
+
+=cut
+
+for my $attr (qw(svn_user svn_password)) {
+    has $attr => (
+        is        => 'ro',
+        isa       => 'Str',
+        lazy      => 1,
+        predicate => "_has_$attr",
+        default   => sub { $ARG[0]->_from_app_config($attr) },
+    );
+}
+
+=attr working_url
+
+URL for the directory currently holding your distribution.  Defaults to your
+distribution's repository location as stated in your C<META.yml> file, or
+the URL associated with the current working copy.
+
+=cut
+
+has 'working_url' => (
+    is         => 'ro',
+    isa        => Uri,
+    coerce     => 1,
+    lazy_build => 1,
+);
+
+sub _build_working_url {
+    my $self = shift;
+    my $url;
+    if ( $url = $self->zilla->distmeta->{resources}{repository} ) {
+        return URI->new($url);
+    }
+    $self->_svn->info( getcwd(), undef, undef,
+        sub { $url = URI->new( $ARG[1]->URL() ) }, 0 );
+    return $url;
+}
+
+has '_base_url' => (
+    is         => 'ro',
+    isa        => Uri,
+    coerce     => 1,
+    lazy_build => 1,
+);
+
+sub _build__base_url {
+    my $self = shift;
+
+    my $url        = $self->working_url->clone();
+    my @segments   = $url->path_segments();
+    my %url_offset = (
+        trunk    => -1,
+        branches => -2,
+    );
+    while ( my ( $segment, $offset ) = each %url_offset ) {
+        if ( $segments[$offset] eq $segment ) {
+            $url->path_segments( @segments[ 0 .. $#segments + $offset ] );
+            return $url;
+        }
+    }
+    $self->_svn->info( getcwd(), undef, undef,
+        sub { $url = URI->new( $ARG[1]->repos_root_URL() ) }, 0 );
+    return $url;
+}
+
+sub _from_app_config {
+    my ( $self, $key ) = @ARG;
+    return unless my $app = $self->zilla->dzil_app();
+    return $app->config_for('Dist::Zilla::App::Command::release')->{$key};
+}
+
+has '_svn' => (
+    is         => 'ro',
+    isa        => 'SVN::Client',
+    lazy_build => 1,
+);
+
+sub _build__svn {
+    my $self = shift;
+## no critic (ProhibitCallsToUnexportedSubs)
+
+    my @auth_baton = (
+        SVN::Client::get_simple_provider(),
+        SVN::Client::get_username_provider(),
+    );
+    if ( $self->_has_svn_user() and $self->_has_svn_password() ) {
+        unshift @auth_baton, SVN::Client::get_simple_prompt_provider(
+            sub {
+                for my $attr (qw(username password)) {
+                    $ARG[0]->$attr( $self->$attr );
+                }
+            },
+            0
+        );
+    }
+
+    my $ctx_ref = SVN::Client->new(
+        auth    => \@auth_baton,
+        log_msg => sub { ${ $ARG[0] } = "[__PACKAGE__]" },
+        notify  => $self->_make_notify_callback(),
+    );
+    return $ctx_ref;
+}
+
+# set up constant hashes of Subversion status codes to names
+Readonly my %_ACTION_NAME => _codes_to_hash('SVN::Wc::Notify::Action');
+Readonly my %_STATE_NAME  => _codes_to_hash('SVN::Wc::Notify::State');
+Readonly my %_NODE_NAME   => _codes_to_hash('SVN::Node');
+
+sub _make_notify_callback {
+    my $self = shift;
+    return sub {
+        my ( $path, $action, $node_kind, $mime, $state, $revision_num )
+            = @ARG;
+
+        $self->log(
+            join(
+                q{ }, '[SVN]',
+                grep {$ARG} (
+                    $_ACTION_NAME{$action},  $_STATE_NAME{$state},
+                    $_NODE_NAME{$node_kind}, $path,
+                    "r$revision_num",
+                ),
+            )
+        );
+        return;
+    };
+}
+
+sub _codes_to_hash {
+    my $package = (shift) . q{::};
+    no strict 'refs';
+    return map { ${ ${$package}{$ARG} } => $ARG }
+        grep { ${ ${$package}{$ARG} } }
+        keys %{$package};
+}
+
+no Moose::Role;
+1;
+
+__END__
+
+=head1 DESCRIPTION
+
+This role is used within the Subversion plugin to provide common attributes
+and defaults.
