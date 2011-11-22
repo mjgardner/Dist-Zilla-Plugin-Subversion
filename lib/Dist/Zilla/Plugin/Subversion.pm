df3f56837250ca29b2b4e8326a9554a167931d0d
Mark Gardner <mjgardner@cpan.org>
2011-11-22 17:22:36 -0500

    add Pod::Weaver settings and fixes

--- lib/Dist/Zilla/Plugin/Subversion.pm
+++ lib/Dist/Zilla/Plugin/Subversion.pm
@@ -1,25 +1,20 @@
 package Dist::Zilla::Plugin::Subversion;
 
-# ABSTRACT: update your Subversion repository after release
-## no critic (ProhibitLongLines)
-
 use Dist::Zilla 4.101550;
 1;
 
-__END__
-
-=encoding utf8
+# ABSTRACT: update your Subversion repository after release
 
 =head1 DESCRIPTION
 
-This set of plugins for L<Dist::Zilla> can do interesting things for
-module authors using L<Subversion|http://subversion.apache.org/> to track
+This set of plugins for L<Dist::Zilla|Dist::Zilla> can do interesting things for
+module authors using Subversion (L<http://subversion.apache.org/>) to track
 their work. The following plugins are provided in this distribution:
 
 =over
 
-=item * L<Dist::Zilla::Plugin::Subversion::ReleaseDist>
+=item * L<Dist::Zilla::Plugin::Subversion::ReleaseDist|Dist::Zilla::Plugin::Subversion::ReleaseDist>
 
-=item * L<Dist::Zilla::Plugin::Subversion::Tag>
+=item * L<Dist::Zilla::Plugin::Subversion::Tag|Dist::Zilla::Plugin::Subversion::Tag>
 
 =back
